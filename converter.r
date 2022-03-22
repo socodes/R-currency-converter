be9bc15017fede7bd3b3c4d76200fcb3afaa0a72
Muhammed Didin <mdidin80@gmail.com>
2022-03-22 22:17:28 +0300

    Take deposit from user

--- converter.r
+++ converter.r
@@ -9,19 +9,22 @@ eur <- "https://free.currconv.com/api/v7/convert?q=EUR_TRY&compact=ultra&apiKey=
 #Get user selection
 print("1) USD to try")
 print("2) EUR to TRY")
-select = readline()
+select = readline("Enter your selection: ")
 select= as.integer(select)
-print(select)
+
 #if user selects usd, make get request to usd link.
 if(select == 1) {
+  deposit = as.integer(readline("How much money you have: "))
   res = GET(usd)
   arr = fromJSON(rawToChar(res$content))
-  print("Current USD to TRY currency: ",arr$USD_TRY)
+  print("Current USD to TRY currency: ",deposit * arr$USD_TRY)
+  
 #if user selects eur, make get request to eur link.
 }else if(select==2){
+  deposit = as.integer(readline("How much money you have: "))
   res = GET(eur)
   arr = fromJSON(rawToChar(res$content))
-  print("Current EUR to TRY currency: ",arr$EUR_TRY)
+  print("Current EUR to TRY currency: ",deposit * arr$USD_TRY)
 }
 
 
