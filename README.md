b75284519443b29f6039c2d9632418bbd7c8d63c
Muhammed Didin <mdidin80@gmail.com>
2022-03-22 22:06:25 +0300

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# R-currency-converter
+# Currency Converter in R langeage
+
+This repository contains a currency converter application written in R language. 
+
+Libraries used:
+- httr
+- jsonlite
--- converter.r
+++ converter.r
@@ -1,19 +1,23 @@
+#import libraries
 library(httr)
 library(jsonlite)
 
+#urls to make request
 usd <- "https://free.currconv.com/api/v7/convert?q=USD_TRY&compact=ultra&apiKey=fb46a2778c4e079f7be5"
 eur <- "https://free.currconv.com/api/v7/convert?q=EUR_TRY&compact=ultra&apiKey=fb46a2778c4e079f7be5"
 
+#Get user selection
 print("1) USD to try")
 print("2) EUR to TRY")
 select = readline()
 select= as.integer(select)
 print(select)
-
+#if user selects usd, make get request to usd link.
 if(select == 1) {
   res = GET(usd)
   arr = fromJSON(rawToChar(res$content))
   print("Current USD to TRY currency: ",arr$USD_TRY)
+#if user selects eur, make get request to eur link.
 }else if(select==2){
   res = GET(eur)
   arr = fromJSON(rawToChar(res$content))
